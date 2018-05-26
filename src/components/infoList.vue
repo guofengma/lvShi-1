82d2d12833bf3a5cac3b8907160c52f523a45d52
zys <907980361@qq.com>
2018-05-26 21:33:16 +0800

    律师公众号

---
+++ src/components/infoList.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="infolist">
+		
+		<!-- 头部 -->
+		<div class="list-head">
+			<input type="search" class="search" name="" placeholder="请输入关键词搜索">
+			<img src="../common/img/search.png" class="search-img">
+			<img src="../common/img/cate.png" class="cate-img">
+		</div>
+
+		<!-- 分类 -->
+		<div class="cate-box">
+			<div class="cate-tit">要问分类查找</div>
+			<div class="cate-con">
+				<span class="cate-item">民事</span>
+			</div>
+		</div>
+
+		<!-- 文章列表 -->
+		<div class="info-list">
+			<div class="info-item">
+				<div class="info-info-box">
+					<div class="info-item-tit">
+						离婚房子怎么分？法院公布十大典型案例
+					</div>
+					<div class="info-other-info">
+						<span class="time">2018年10月23日</span>
+						<span class="yd">阅读1500</span>
+						<span class="dz">
+							<img src="../common/img/dz.png" class="dz">
+							100
+						</span>
+					</div>
+				</div>
+				<div class="img-box">
+					<img src="../common/img/wzimg.jpg" class="info-img">
+				</div>
+			</div>
+		</div>
+
+	</div>
+</template>
+<style>
+	
+</style>
+<script>
+	export default{
+		props:{},
+		data(){
+			return{
+
+			}
+		},
+
+	}
+</script>
