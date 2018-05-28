9dddc13e52de050d8fba64a6570049a9cc78feed
zhuyuesen <zhuyuesen>
2018-05-28 23:49:56 +0800

    文章详情页面

---
+++ src/components/infoDetail.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="page">
+		<div class="article-box">
+			<div class="atr-tit"></div>
+			<div class="art-info">
+				<span class="time"></span>
+				<span class="author"></span>
+			</div>
+			<div class="art-con">
+				
+			</div>
+		</div>
+		<!-- 点赏文章 -->
+		<div class="art-shang">
+			<img src="../common/img/shang.png">
+			<span class="shang-name">点赏文章</span>
+		</div>
+		<!-- 二维码 -->
+		<div class="code-box">
+			<div class="code-item">
+				<img src="../common/img/code.png">
+				<span class="name">扫描关注“律师公众号” 您专业的律师顾问</span>
+			</div>
+			<div class="code-item">
+				<img src="../common/img/code.png">
+				<span class="name">扫描关注“律师小程序” 您专业的律师顾问</span>
+			</div>
+		</div>
+
+		<!-- 阅 赞 评-->
+		<div class="handle-box">
+			<span class="yue">阅读</span>
+			<span class="zan">
+				<img src="../common/img/zan.png">
+				100
+			</span>
+			<span class="ping">
+				<img src="../common/img/bz.png">
+				评论
+			</span>
+		</div>
+
+	</div>
+</template>
